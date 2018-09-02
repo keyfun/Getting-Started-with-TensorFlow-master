08e1bed2ab33c8bfadd1393ead33f097dcf27efa
Key Hui <>
2018-09-03 00:21:20 +0800

    no message

--- Chapter 2/defining_tensors.py
+++ Chapter 2/defining_tensors.py
@@ -1,6 +1,9 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import numpy as np
 
-tensor_1d = np.array([1.3,1,4.0,23.99])
+tensor_1d = np.array([1.3, 1, 4.0, 23.99])
 
 print tensor_1d
 
@@ -18,12 +21,13 @@ with tf.Session() as sess:
     print sess.run(tf_tensor[2])
 
 
-tensor_2d=np.array([(1,2,3,4),(4,5,6,7),(8,9,10,11),(12,13,14,15)])
+tensor_2d = np.array(
+    [(1, 2, 3, 4), (4, 5, 6, 7), (8, 9, 10, 11), (12, 13, 14, 15)])
 
 print tensor_2d
 print tensor_2d[3][3]
-print tensor_2d[0:2,0:2]
+print tensor_2d[0:2, 0:2]
 
-tf_tensor=tf.placeholder("float64",tensor_2d,name='x')
+tf_tensor = tf.placeholder("float64", tensor_2d, name='x')
 with tf.Session() as sess:
     print sess.run(x)
--- Chapter 2/random_numbers.py
+++ Chapter 2/random_numbers.py
@@ -1,3 +1,6 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
@@ -5,15 +8,15 @@ import matplotlib.pyplot as plt
 # 0 and standard deviation 2.
 norm = tf.random_normal([100], mean=0, stddev=2)
 with tf.Session() as session:
-    plt.hist(norm.eval(),normed=True)
-    plt.show()  
+    plt.hist(norm.eval(), normed=True)
+    plt.show()
 
 
-uniform = tf.random_uniform([100],minval=0,maxval=1,dtype=tf.float32)
+uniform = tf.random_uniform([100], minval=0, maxval=1, dtype=tf.float32)
 with tf.Session() as session:
     print uniform.eval()
-    plt.hist(uniform.eval(),normed=True)
-    plt.show() 
+    plt.hist(uniform.eval(), normed=True)
+    plt.show()
 
 
 uniform_with_seed = tf.random_uniform([1], seed=1)
@@ -23,42 +26,41 @@ uniform_without_seed = tf.random_uniform([1])
 # sequence of values for 'a', but different sequences of values for 'b'.
 print("First Run")
 with tf.Session() as first_session:
-  print("uniform with (seed = 1) = {}"\
-        .format(first_session.run(uniform_with_seed)))  
-  print("uniform with (seed = 1) = {}"\
-        .format(first_session.run(uniform_with_seed)))
-  print("uniform without seed = {}"\
-        .format(first_session.run(uniform_without_seed)))  
-  print("uniform without seed = {}"\
-        .format(first_session.run(uniform_without_seed)))  
+    print("uniform with (seed = 1) = {}"
+          .format(first_session.run(uniform_with_seed)))
+    print("uniform with (seed = 1) = {}"
+          .format(first_session.run(uniform_with_seed)))
+    print("uniform without seed = {}"
+          .format(first_session.run(uniform_without_seed)))
+    print("uniform without seed = {}"
+          .format(first_session.run(uniform_without_seed)))
 
 print("Second Run")
 with tf.Session() as second_session:
-  print("uniform with (seed = 1) = {}"\
-        .format(second_session.run(uniform_with_seed)))  
-  print("uniform with (seed = 1) = {}"\
-        .format(second_session.run(uniform_with_seed)))  
-  print("uniform without seed = {}"\
-        .format(second_session.run(uniform_without_seed)))  
-  print("uniform without seed = {}"\
-        .format(second_session.run(uniform_without_seed)))
-
+    print("uniform with (seed = 1) = {}"
+          .format(second_session.run(uniform_with_seed)))
+    print("uniform with (seed = 1) = {}"
+          .format(second_session.run(uniform_with_seed)))
+    print("uniform without seed = {}"
+          .format(second_session.run(uniform_without_seed)))
+    print("uniform without seed = {}"
+          .format(second_session.run(uniform_without_seed)))
 
 
 import tensorflow as tf
 
 trials = 100
 hits = 0
-x = tf.random_uniform([1],minval=-1,maxval=1,dtype=tf.float32)
-y = tf.random_uniform([1],minval=-1,maxval=1,dtype=tf.float32)
+x = tf.random_uniform([1], minval=-1, maxval=1, dtype=tf.float32)
+y = tf.random_uniform([1], minval=-1, maxval=1, dtype=tf.float32)
 pi = []
 sess = tf.Session()
 with sess.as_default():
-    for i in range(1,trials):
-        for j in range(1,trials):
-            if x.eval()**2 + y.eval()**2 < 1 :
+    for i in range(1, trials):
+        for j in range(1, trials):
+            if x.eval()**2 + y.eval()**2 < 1:
                 hits = hits + 1
-                pi.append((4 * float(hits) / i)/trials)  
+                pi.append((4 * float(hits) / i)/trials)
 
 plt.plot(pi)
 plt.show()
