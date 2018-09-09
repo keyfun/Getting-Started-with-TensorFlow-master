1a96c6b9c500dd1e6635023b9a73ff08e9118619
Key Hui <>
2018-09-09 18:00:38 +0800

    format code

--- Chapter 2/random_numbers.py
+++ Chapter 2/random_numbers.py
@@ -1,8 +1,9 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-import tensorflow as tf
 import matplotlib.pyplot as plt
+import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Create a tensor of shape [100] consisting of random normal values, with mean
 # 0 and standard deviation 2.
@@ -11,13 +12,11 @@ with tf.Session() as session:
     plt.hist(norm.eval(), normed=True)
     plt.show()
 
-
 uniform = tf.random_uniform([100], minval=0, maxval=1, dtype=tf.float32)
 with tf.Session() as session:
-    print uniform.eval()
+    print(uniform.eval())
     plt.hist(uniform.eval(), normed=True)
     plt.show()
-
 
 uniform_with_seed = tf.random_uniform([1], seed=1)
 uniform_without_seed = tf.random_uniform([1])
@@ -46,7 +45,6 @@ with tf.Session() as second_session:
     print("uniform without seed = {}"
           .format(second_session.run(uniform_without_seed)))
 
-
 import tensorflow as tf
 
 trials = 100
@@ -58,9 +56,9 @@ sess = tf.Session()
 with sess.as_default():
     for i in range(1, trials):
         for j in range(1, trials):
-            if x.eval()**2 + y.eval()**2 < 1:
+            if x.eval() ** 2 + y.eval() ** 2 < 1:
                 hits = hits + 1
-                pi.append((4 * float(hits) / i)/trials)
+                pi.append((4 * float(hits) / i) / trials)
 
 plt.plot(pi)
 plt.show()
