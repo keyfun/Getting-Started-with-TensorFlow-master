7f119c04e65d5308349da1ee9d5358c65ce9899b
Key Hui <>
2018-09-09 23:06:02 +0800

    update ch2 sample

--- Chapter 2/handling_tensors.py
+++ Chapter 2/handling_tensors.py
@@ -1,30 +1,26 @@
-#handling tensors
-
-
-#STEP 1 --- PREPARE THE DATA
+# handling tensors
 import matplotlib.image as mp_image
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# STEP 1 --- PREPARE THE DATA
 filename = "packt.jpeg"
 input_image = mp_image.imread(filename)
 
-#dimension
-print 'input dim = {}'.format(input_image.ndim)
-#shape
-print 'input shape = {}'.format(input_image.shape)
+# dimension
+print('input dim = {}'.format(input_image.ndim))
+# shape
+print('input shape = {}'.format(input_image.shape))
 
-import matplotlib.pyplot as plt
 plt.imshow(input_image)
 plt.show()
 
-import tensorflow as tf
-
-my_image = tf.placeholder("uint8",[None,None,3])
-slice = tf.slice(my_image,[10,0,0],[16,-1,-1])
-
+my_image = tf.placeholder("uint8", [None, None, 3])
+slice = tf.slice(my_image, [10, 0, 0], [16, -1, -1])
 
 with tf.Session() as session:
-    result = session.run(slice,feed_dict={my_image: input_image})
+    result = session.run(slice, feed_dict={my_image: input_image})
     print(result.shape)
 
 plt.imshow(result)
 plt.show()
-
--- Chapter 2/handling_tensors_III.py
+++ Chapter 2/handling_tensors_III.py
@@ -1,32 +1,29 @@
-#handling tensors second example
-
-
-#STEP 1 --- PREPARE THE DATA
+# handling tensors second example
 import matplotlib.image as mp_image
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# STEP 1 --- PREPARE THE DATA
 filename = "packt.jpeg"
 input_image = mp_image.imread(filename)
 
-#dimension
-print 'input dim = {}'.format(input_image.ndim)
-#shape
-print 'input shape = {}'.format(input_image.shape)
+# dimension
+print('input dim = {}'.format(input_image.ndim))
+# shape
+print('input shape = {}'.format(input_image.shape))
 
-height,width,depth= input_image.shape
+height, width, depth = input_image.shape
 
-import matplotlib.pyplot as plt
 plt.imshow(input_image)
 plt.show()
 
-import tensorflow as tf
-
-x = tf.Variable(input_image,name='x')
+x = tf.Variable(input_image, name='x')
 model = tf.initialize_all_variables()
 
 with tf.Session() as session:
-    x = tf.transpose(x, perm=[1,0,2])
+    x = tf.transpose(x, perm=[1, 0, 2])
     session.run(model)
-    result=session.run(x)
+    result = session.run(x)
 
 plt.imshow(result)
 plt.show()
-
